84a49ebafaf7e71348db5614e98acccc3fc949dd
Ole Fredrik Borgundvåg Berg <ole.fredrik.berg@gmail.com>
2020-10-30 15:52:35 +0100

    Added build

---
+++ build/precache-manifest.a97fce4d162587cefd02814ab304687b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed3839d261048ebd2d0c87b75be81d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10141252445058cb9df",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0730e026191ffdde4cb0",
+    "url": "/static/css/main.ab7ad991.chunk.css"
+  },
+  {
+    "revision": "f10141252445058cb9df",
+    "url": "/static/js/2.b3778509.chunk.js"
+  },
+  {
+    "revision": "01431f43f6c7c7813effa35994181ef1",
+    "url": "/static/js/2.b3778509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0730e026191ffdde4cb0",
+    "url": "/static/js/main.0b85bea2.chunk.js"
+  },
+  {
+    "revision": "47da8c6646fbd157548a",
+    "url": "/static/js/runtime-main.086108ee.js"
+  }
+]);
