c9d1588219fbccce214729ae34321f71c49f7b25
Albert Johannessen <albertjohannessen@live.no>
2020-11-06 15:50:45 +0100

    Cleaned branch

--- routes/spicyhello.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET megagay nudes. */
-router.get('/', function(req, res, next) {
-  res.send('nå blir d hanska');
-});
-
-module.exports = router;
